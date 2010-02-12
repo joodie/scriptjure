9091746ea2a69e0b82a389aae3b30d57a1ed5361
Joost Diepenmaat <joost@zeekat.nl>
2010-02-12 14:52:30 +0100

    indentation fixes

--- src/com/reasonr/scriptjure.clj
+++ src/com/reasonr/scriptjure.clj
@@ -31,7 +31,7 @@
   (:use clojure.walk))
 
 (defmulti emit (fn [ expr ] (do ; (println "emit-dispatch: " expr (type expr)) 
-				(type expr))))
+                                (type expr))))
 
 (defmulti emit-special (fn [ & args] (identity (first args))))
 
@@ -98,9 +98,9 @@
        (emit true-form)
        "\n }"
        (when (first false-form)
-	 (str " else { \n"
-	      (emit (first false-form))
-	      " }"))))
+         (str " else { \n"
+              (emit (first false-form))
+              " }"))))
        
 (defmethod emit-special 'dot-method [type [method obj & args]]
   (let [method (symbol (str/drop (str method) 1))]
@@ -132,11 +132,11 @@
 (defmethod emit-special 'fn [type [fn & expr]]
   (if (symbol? (first expr))
     (let [name (first expr)
-	  signature (second expr)
-	  body (rest (rest expr))]
+          signature (second expr)
+          body (rest (rest expr))]
       (emit-function name signature body))
     (let [signature (first expr)
-	  body (rest expr)]
+          body (rest expr)]
       (emit-function nil signature body))))
 
 (defmethod emit clojure.lang.Cons [expr]
@@ -145,12 +145,12 @@
 (defmethod emit clojure.lang.IPersistentList [expr]
   (if (symbol? (first expr))
     (let [head (symbol (name (first expr)))  ; remove any ns resolution
-	  expr (conj (rest expr) head)]
+          expr (conj (rest expr) head)]
       (cond 
-	(and (= (str/get (str head) 0) \.) (> (count (str head)) 1)) (emit-special 'dot-method expr)
-	(special-form? head) (emit-special head expr)
-	(infix-operator? head) (emit-infix head expr)
-	:else (emit-special 'funcall expr)))
+        (and (= (str/get (str head) 0) \.) (> (count (str head)) 1)) (emit-special 'dot-method expr)
+        (special-form? head) (emit-special head expr)
+        (infix-operator? head) (emit-infix head expr)
+        :else (emit-special 'funcall expr)))
     (throw (new Exception (str "invalid form: " expr)))))
 
 (defmethod emit clojure.lang.IPersistentVector [expr]
@@ -165,8 +165,8 @@
 
 (defn _js [forms]
   (let [code (if (> (count forms) 1)
-	       (emit-do forms)
-	       (emit (first forms)))]
+               (emit-do forms)
+               (emit (first forms)))]
     ;(println "js " forms " => " code)
     code))
 
